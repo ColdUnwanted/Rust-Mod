cb1b6766239ea78a0428ee56e66b96184e41a474
ColdUnwanted <37330208+ColdUnwanted@users.noreply.github.com>
2021-02-19 12:19:37 +0800

    Initial commit

---
+++ Centials/Centials/Centials.cs
@@ -0,0 +1,12 @@
+﻿namespace Oxide.Plugins
+{
+    [Info("Centials", "ColdUnwanted", 0.1)]
+    [Description("Everything you need in one plugin")]
+    public class Centials : RustPlugin
+    {
+        public void Init()
+        {
+            ConVar.
+        }
+    }
+}
